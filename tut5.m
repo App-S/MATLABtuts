0292c634ab818498a2fdb74a15fb2352229492cb
App-S <apurvasijaria.iitkgp@gmail.com>
2016-06-25 13:55:10 +0530

    'tutorial'

---
+++ tut5.m
@@ -0,0 +1,29 @@
+% Commands
+% % log(n) Calculates the natural logarithm (base e) of n.
+% % semilogy(x,y) Graphs a plot of y vs. x using a logarithmic scale (powers of ten) on
+% % the y-axis.
+% % semilogx(x,y) Graphs a plot of y vs. x using a logarithmic scale (powers of ten) on
+% % the x-axis.
+% % loglog(x,y) Graphs a plot of y vs. x using a logarithmic scale (powers of ten) on
+% % both axes. The logarithmic scales prove most useful when the value
+% % spans multiple orders of magnitude
+
+% Graph the efficiency of several programming algorithms according to big-O notation, a method
+% of describing the running time of algorithms. Each expression represents the scale by which an
+% algorithm’s computation time increases as the number of its input elements increases. For example,
+% O(n) represents an algorithm that scales linearly, so that its computation time increases at the
+% same rate as the number of elements. The algorithms you must graph have the following big-O
+% characteristics:
+% Algorithm #1: O(n)
+% Algorithm #2: O(n2)
+% Algorithm #3: O(n3)
+% Algorithm #4: O(2n)
+% Algorithm #5: O(en)
+% After generating an initial graph with ranging from 0 to 8, use logarithmic scaling on the y-axis
+% of a second graph to make it more readable. You can also use the mouse to change the y-axis
+% scale. Go to the main menu of the figure, click Edit>Axes Properties. . . , the property editor dialogue
+% will pop out. There, you can also change the font, the range of the axes, . . . Try to play with it.
+
+
+
+
