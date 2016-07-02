bbee58fde7e3b3ae0d2895afe8cd8cb8dd41ade9
App-S <apurvasijaria.iitkgp@gmail.com>
2016-07-02 13:17:45 +0530

    'add'

--- ring.m
+++ ring.m
@@ -1,7 +1,10 @@
 nodes=6;
 networks=1;
-weighted= 'skew' %'random' 'uniform'
-
+weighted= 'uniform' %'skew' %'random'
+alphaAll = fliplr([.01:.01:.99]);
+alpha = alphaAll(1:length(alphaAll));
+ p = 1-alpha;
+Q=p;
 switch weighted
     case {'uniform'}
         weight=ones(nodes)
@@ -14,14 +17,22 @@ switch weighted
     weight(:) = W(p);
 end
 
-for j= 1:networks
-    start= zeros(nodes);
-    network= ones(nodes).*((1-eye(nodes)));
-    for i= 1:nodes-1
-        start(i+1,i)=1;
-    
-    end
-    start(1,nodes)=1;
-    networkSys= network.*start.*weight;
-end
+  for i = 1:networks
+            network = zeros(nodes);
+            s = rand(nodes*nodes,1);
+            s(s<randi(numel(Q)) = 0;
+            s(:) = ceil(s);
+            network(:) = s;
+            networkSys(:,:,i) = network.*weight.*abs(eye(nodes)-1);
+        end  
+% for j= 1:networks
+%     start= zeros(nodes);
+%     network= ones(nodes).*((1-eye(nodes)));
+%     for i= 1:nodes-1
+%         start(i+1,i)=1;
+%     
+%     end
+%     start(1,nodes)=1;
+%     networkSys= network.*start.*weight;
+% end
 networkSys
